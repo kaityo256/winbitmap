092e95064107e453322a808364b3de854e57bd0e
H. Watanabe <kaityo@users.sourceforge.jp>
2019-02-04 20:43:56 +0900

    updates .gitignore

--- bm.hpp
+++ bm.hpp
@@ -1,209 +1,198 @@
 /*
-   Copyright (C) 2013 - present, Hiroshi Watanabe
-   All rights reserved.
+MIT License
 
-   Redistribution and use in source and binary forms,
-   with or without modification, are permitted provided
-   that the following conditions are met:
+Copyright (c) 2019 - present H. Watanabe
 
- * Redistributions of source code must retain the above copyright
- notice, this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above
- copyright notice, this list of conditions and the following
- disclaimer in the documentation and/or other materials
- provided with the distribution.
- * Neither the name of <organization> nor the names of its
- contributors may be used to endorse or promote products derived
- from this software without specific prior written permission.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
- THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
 #pragma once
 
-#include <iostream>
 #include <fstream>
+#include <iostream>
 #include <vector>
 
 typedef unsigned char BYTE;
 typedef unsigned short WORD;
 typedef unsigned int DWORD;
 
-class WindowsBitmap{
-  private:
-    int Width, Height,Line;
-    //BYTE * ImageBuffer;
-    std::vector<BYTE> ImageBuffer;
-    int BufferSize;
-    int CX, CY; // Current Point
-    BYTE R,G,B;
+class WindowsBitmap {
+private:
+  int Width, Height, Line;
+  std::vector<BYTE> ImageBuffer;
+  int BufferSize;
+  int CX, CY; // Current Point
+  BYTE R, G, B;
 
-  public:
-    WindowsBitmap(int w, int h){
-      Width = w;
-      Height = h;
-      Line = ((w*3 -1)/4)*4 + 4; 
-      BufferSize = Line*h;
-      ImageBuffer.resize(BufferSize, 0);
-      /*
-      for(int i=0;i<BufferSize;i++){
-        ImageBuffer[i] = 0;
-      }*/
-      CX = 0;
-      CY = 0;
+public:
+  WindowsBitmap(int w, int h) {
+    Width = w;
+    Height = h;
+    Line = ((w * 3 - 1) / 4) * 4 + 4;
+    BufferSize = Line * h;
+    ImageBuffer.resize(BufferSize, 0);
+    CX = 0;
+    CY = 0;
+  }
 
-    }
+  void MoveTo(int x, int y) {
+    CX = x;
+    CY = y;
+  }
 
-    void MoveTo(int x, int y){
-      CX=x;
-      CY=y;
-    }
+  void LineTo(int x, int y) {
+    int dx = (x > CX) ? x - CX : CX - x;
+    int dy = (y > CY) ? y - CY : CY - y;
 
-    void LineTo(int x, int y){
-      int dx = ( x > CX ) ? x - CX : CX - x;
-      int dy = ( y > CY ) ? y - CY : CY - y;
+    int sx = (x > CX) ? 1 : -1;
+    int sy = (y > CY) ? 1 : -1;
 
-      int sx = ( x > CX ) ? 1 : -1;
-      int sy = ( y > CY ) ? 1 : -1;
-
-      if ( dx > dy ) {
-        int E = -dx;
-        for (int i = 0 ; i <= dx ; i++ ) {
-          DrawPoint(CX,CY);
-          CX += sx;
-          E += 2 * dy;
-          if ( E >= 0 ) {
-            CY += sy;
-            E -= 2 * dx;
-          }
-        }
-      } else {
-        int E = -dy;
-        for (int i = 0 ; i <= dy ; i++ ) {
-          DrawPoint(CX,CY);
+    if (dx > dy) {
+      int E = -dx;
+      for (int i = 0; i <= dx; i++) {
+        DrawPoint(CX, CY);
+        CX += sx;
+        E += 2 * dy;
+        if (E >= 0) {
           CY += sy;
-          E += 2 * dx;
-          if ( E >= 0 ) {
-            CX += sx;
-            E -= 2 * dy;
-          }
+          E -= 2 * dx;
+        }
+      }
+    } else {
+      int E = -dy;
+      for (int i = 0; i <= dy; i++) {
+        DrawPoint(CX, CY);
+        CY += sy;
+        E += 2 * dx;
+        if (E >= 0) {
+          CX += sx;
+          E -= 2 * dy;
         }
       }
     }
+  }
 
-    void DrawPoint(int x, int y){
-      if(x <0 || x >= Width)return;
-      if(y <0 || y >= Height)return;
-      int p = y*Line+x*3;
-      ImageBuffer[p] = B;
-      ImageBuffer[p+1] = G;
-      ImageBuffer[p+2] = R;
-    }
+  void DrawPoint(int x, int y) {
+    if (x < 0 || x >= Width)
+      return;
+    if (y < 0 || y >= Height)
+      return;
+    int p = y * Line + x * 3;
+    ImageBuffer[p] = B;
+    ImageBuffer[p + 1] = G;
+    ImageBuffer[p + 2] = R;
+  }
 
-    void SetColor(BYTE red,BYTE green,BYTE blue){
-      R = red;
-      G = green;
-      B = blue;
-    }
+  void SetColor(BYTE red, BYTE green, BYTE blue) {
+    R = red;
+    G = green;
+    B = blue;
+  }
 
-    void FillRect(int x, int y, int w, int h){
-      for(int iy=0;iy<h;iy++){
-        for(int ix=0;ix<w;ix++){
-          DrawPoint(ix + x, iy + y);
-        }
+  void FillRect(int x, int y, int w, int h) {
+    for (int iy = 0; iy < h; iy++) {
+      for (int ix = 0; ix < w; ix++) {
+        DrawPoint(ix + x, iy + y);
       }
     }
-    
-    void FillCircle(int x0, int y0, int r){
-      int x = r;
-      int y = 0;
-      int F = -2 * r + 3;
-      while ( x >= y ) {
-        for(int i=-x;i<=x;i++){
-          DrawPoint( x0 + i, y0 + y);
-          DrawPoint( x0 + i, y0 - y);
-          DrawPoint( x0 + y, y0 + i);
-          DrawPoint( x0 - y, y0 + i);
-        }
-        if ( F >= 0 ) {
-          x--;
-          F -= 4 * x;
-        }
-        y++;
-        F += 4 * y + 2;
+  }
+
+  void FillCircle(int x0, int y0, int r) {
+    int x = r;
+    int y = 0;
+    int F = -2 * r + 3;
+    while (x >= y) {
+      for (int i = -x; i <= x; i++) {
+        DrawPoint(x0 + i, y0 + y);
+        DrawPoint(x0 + i, y0 - y);
+        DrawPoint(x0 + y, y0 + i);
+        DrawPoint(x0 - y, y0 + i);
       }
-    }
-
-    void DrawCircle(int x0, int y0, int r){
-      int x = r;
-      int y = 0;
-      int F = -2 * r + 3;
-      while ( x >= y ) {
-        DrawPoint( x0 + x, y0 + y);
-        DrawPoint( x0 - x, y0 + y);
-        DrawPoint( x0 + x, y0 - y);
-        DrawPoint( x0 - x, y0 - y);
-        DrawPoint( x0 + y, y0 + x);
-        DrawPoint( x0 - y, y0 + x);
-        DrawPoint( x0 + y, y0 - x);
-        DrawPoint( x0 - y, y0 - x);
-        if ( F >= 0 ) {
-          x--;
-          F -= 4 * x;
-        }
-        y++;
-        F += 4 * y + 2;
+      if (F >= 0) {
+        x--;
+        F -= 4 * x;
       }
+      y++;
+      F += 4 * y + 2;
     }
-    
-    void SaveToFile(const char *filename){
-      DWORD bfSize = BufferSize+54;
-      WORD bfReserved1 = 0;
-      WORD bfReserved2 = 0;
-      DWORD bfOffBits = 54;
+  }
 
-      DWORD biSize = 40;
-      DWORD biWidth = Width;
-      DWORD biHeight = Height;
-      WORD biPlanes = 1;
-      WORD biBitCount = 24;
-      DWORD biCompression = 0;
-      DWORD biSizeImage = 0;
-      DWORD biXPelsPerMeter = 0;
-      DWORD biYPelsPerMeter = 0;
-      DWORD biClrUsed = 0;
-      DWORD biClrImportant= 0;
-
-      std::ofstream fs(filename);
-      //BITMAPFILEHEADER
-      fs.write("BM",sizeof(WORD));
-      fs.write((char *)&bfSize,sizeof(DWORD));
-      fs.write((char *)&bfReserved1,sizeof(WORD));
-      fs.write((char *)&bfReserved2,sizeof(WORD));
-      fs.write((char *)&bfOffBits,sizeof(DWORD));
-      //BITMAPINFOHEADER
-      fs.write((char *)&biSize,sizeof(DWORD));
-      fs.write((char *)&biWidth,sizeof(DWORD));
-      fs.write((char *)&biHeight,sizeof(DWORD));
-      fs.write((char *)&biPlanes,sizeof(WORD));
-      fs.write((char *)&biBitCount,sizeof(WORD));
-      fs.write((char *)&biCompression,sizeof(DWORD));
-      fs.write((char *)&biSizeImage,sizeof(DWORD));
-      fs.write((char *)&biXPelsPerMeter,sizeof(DWORD));
-      fs.write((char *)&biYPelsPerMeter,sizeof(DWORD));
-      fs.write((char *)&biClrUsed,sizeof(DWORD));
-      fs.write((char *)&biClrImportant,sizeof(DWORD));
-      //DATA
-      fs.write((char *)ImageBuffer.data(),BufferSize);
-      fs.close();
+  void DrawCircle(int x0, int y0, int r) {
+    int x = r;
+    int y = 0;
+    int F = -2 * r + 3;
+    while (x >= y) {
+      DrawPoint(x0 + x, y0 + y);
+      DrawPoint(x0 - x, y0 + y);
+      DrawPoint(x0 + x, y0 - y);
+      DrawPoint(x0 - x, y0 - y);
+      DrawPoint(x0 + y, y0 + x);
+      DrawPoint(x0 - y, y0 + x);
+      DrawPoint(x0 + y, y0 - x);
+      DrawPoint(x0 - y, y0 - x);
+      if (F >= 0) {
+        x--;
+        F -= 4 * x;
+      }
+      y++;
+      F += 4 * y + 2;
     }
+  }
+
+  void SaveToFile(const char *filename) {
+    DWORD bfSize = ImageBuffer.size() + 54;
+    WORD bfReserved1 = 0;
+    WORD bfReserved2 = 0;
+    DWORD bfOffBits = 54;
+
+    DWORD biSize = 40;
+    DWORD biWidth = Width;
+    DWORD biHeight = Height;
+    WORD biPlanes = 1;
+    WORD biBitCount = 24;
+    DWORD biCompression = 0;
+    DWORD biSizeImage = 0;
+    DWORD biXPelsPerMeter = 0;
+    DWORD biYPelsPerMeter = 0;
+    DWORD biClrUsed = 0;
+    DWORD biClrImportant = 0;
+
+    std::ofstream fs(filename);
+    // BITMAPFILEHEADER
+    fs.write("BM", sizeof(WORD));
+    fs.write((char *)&bfSize, sizeof(DWORD));
+    fs.write((char *)&bfReserved1, sizeof(WORD));
+    fs.write((char *)&bfReserved2, sizeof(WORD));
+    fs.write((char *)&bfOffBits, sizeof(DWORD));
+    // BITMAPINFOHEADER
+    fs.write((char *)&biSize, sizeof(DWORD));
+    fs.write((char *)&biWidth, sizeof(DWORD));
+    fs.write((char *)&biHeight, sizeof(DWORD));
+    fs.write((char *)&biPlanes, sizeof(WORD));
+    fs.write((char *)&biBitCount, sizeof(WORD));
+    fs.write((char *)&biCompression, sizeof(DWORD));
+    fs.write((char *)&biSizeImage, sizeof(DWORD));
+    fs.write((char *)&biXPelsPerMeter, sizeof(DWORD));
+    fs.write((char *)&biYPelsPerMeter, sizeof(DWORD));
+    fs.write((char *)&biClrUsed, sizeof(DWORD));
+    fs.write((char *)&biClrImportant, sizeof(DWORD));
+    // DATA
+    fs.write((char *)ImageBuffer.data(), BufferSize);
+    fs.close();
+  }
 };
--- draw3d.hpp
+++ draw3d.hpp
@@ -1,70 +1,40 @@
-/*
-  Copyright (C) 2013, Hiroshi Watanabe
-  All rights reserved.
-
-  Redistribution and use in source and binary forms,
-  with or without modification, are permitted provided
-  that the following conditions are met:
-
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above
-      copyright notice, this list of conditions and the following
-      disclaimer in the documentation and/or other materials
-      provided with the distribution.
-    * Neither the name of <organization> nor the names of its
-      contributors may be used to endorse or promote products derived
-      from this software without specific prior written permission.
-
-  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-  HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-#ifndef draw3d_h
-#define draw3d_h
+#pragma once
 #include "bm.hpp"
-//----------------------------------------------------------------------
-struct Point{
-  int X, Y; 
+
+struct Point {
+  int X, Y;
   Point();
-  Point(int _x, int _y){
-    X = _x;Y = _y;
-  };
+  Point(int _x, int _y) {
+    X = _x;
+    Y = _y;
+  }
 };
-//----------------------------------------------------------------------
-class Draw3D{
-  private:
-    WindowsBitmap *canvas;
-    int particleNumber;
-    double L;
-    double Theta, Phi;
-    double Sigma;
-    static double *depth;
-    int *sorted_index;
-    int SX, SY;
-    int Width, Height;
-    double Magnification;
-    Point GetPoint(double x, double y, double z);
-    void DrawLine(double x1, double y1, double z1,double x2, double y2, double z2);
-    void DrawAxis(void);
-    void SortParticles(void);
-  public:
-    Draw3D(double ss, int is);
-    ~Draw3D(void);
-    void SetPhi(double p){Phi = p;};
-    void SetTheta(double t){Theta = t;};
-    void SetSigma(double s){Sigma = s;};
-    void Draw(int n, double *qx, double *qy, double *qz);
-    void SaveToFile(const char *filename){canvas->SaveToFile(filename);};
-    static bool Compare(int i, int j);
+
+class Draw3D {
+private:
+  WindowsBitmap *canvas;
+  int particleNumber;
+  double L;
+  double Theta, Phi;
+  double Sigma;
+  static double *depth;
+  int *sorted_index;
+  int SX, SY;
+  int Width, Height;
+  double Magnification;
+  Point GetPoint(double x, double y, double z);
+  void DrawLine(double x1, double y1, double z1, double x2, double y2,
+                double z2);
+  void DrawAxis(void);
+  void SortParticles(void);
+
+public:
+  Draw3D(double ss, int is);
+  ~Draw3D(void);
+  void SetPhi(double p) { Phi = p; };
+  void SetTheta(double t) { Theta = t; };
+  void SetSigma(double s) { Sigma = s; };
+  void Draw(int n, double *qx, double *qy, double *qz);
+  void SaveToFile(const char *filename) { canvas->SaveToFile(filename); };
+  static bool Compare(int i, int j);
 };
-//----------------------------------------------------------------------
-#endif
